bafcd557fbe651f2fbfefc19d65149baf87bed4f
gabs1m <gabs1machado@gmail.com>
2024-03-27 07:42:29 -0300

    feat: add class-validation

--- src/routes/users/dto/create-user.dto.ts
+++ src/routes/users/dto/create-user.dto.ts
@@ -1,8 +1,14 @@
+import { IsEmail, IsNotEmpty, Min, isNotEmpty } from "class-validator";
 import { Classes } from "src/interfaces/IClasses";
 
 export class CreateUserDto {
+    @IsNotEmpty()
     name: string;
+    @IsNotEmpty()
+    @IsEmail()
     email: string;
+    @IsNotEmpty()
+    @Min(8)
     password: string;
     icon?: string;
     permission: string;
